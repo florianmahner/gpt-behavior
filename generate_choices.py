cceaf3f3179c83b408bf45106c6b59bea1ecf0a5
florianmahner <florian.mahner@icloud.com>
2024-01-11 14:39:27 +0100

    Save all hparams and metadata and ensure consistency across words and images

--- generate_choices.py
+++ generate_choices.py
@@ -3,13 +3,71 @@ import pandas as pd
 import os
 from PIL import Image
 import base64
-import requests
 import io
 import toml
 import random
+import pickle
 import copy
 from typing import Generator
 from openai import OpenAI
+from tomlparse import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--image_dir",
+        type=str,
+        default="./data/images/",
+        help="Path to the directory containing the images",
+    )
+    parser.add_argument(
+        "--train_fp",
+        type=str,
+        default="./data/trainset.txt",
+        help="Path to the file containing the image triplets",
+    )
+    parser.add_argument(
+        "--concepts_fp",
+        type=str,
+        default="./data/things_concepts.tsv",
+        help="Path to the file containing the concepts",
+    )
+    parser.add_argument(
+        "--non_unique_words_fp",
+        type=str,
+        default="./data/NonUniqueWords_edited.xlsx",
+        help="Path to the file containing the non-unique words",
+    )
+    parser.add_argument(
+        "--prompt_images_fp",
+        type=str,
+        default="./data/prompt_specifics_images.toml",
+        help="Path to the file containing the prompt specifics for the images",
+    )
+    parser.add_argument(
+        "--prompt_words_fp",
+        type=str,
+        default="./data/prompt_specifics_words.toml",
+        help="Path to the file containing the prompt specifics for the words",
+    )
+    parser.add_argument(
+        "--output_fp",
+        type=str,
+        default="./data/gpt4_choice_words_images.pkl",
+        help="Path to the file containing the output",
+    )
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    parser.add_argument("--temperature", type=float, default=0.5, help="Temperature")
+    parser.add_argument("--image_size", type=int, default=64, help="Image size")
+    parser.add_argument(
+        "--max_triplets",
+        type=int,
+        default=1000,
+        help="Max number of triplets to sample",
+    )
+
+    return parser.parse_args()
 
 
 def parse_non_unique_words(path: str) -> list:
@@ -19,30 +77,24 @@ def parse_non_unique_words(path: str) -> list:
     return non_unique
 
 
-def encode_image(image):
-    buffer = io.BytesIO()
-    image.save(buffer, format="PNG")
-    byte_data = buffer.getvalue()
-    base64_str = base64.b64encode(byte_data)
-    return base64_str.decode("utf-8")
-
-
 def compute_cost(prompt_tokens: int, completion_tokens: int) -> float:
     return (prompt_tokens / 1000) * 0.01 + (completion_tokens / 1000) * 0.03
 
 
 def dataloader(
     image_dir: str,
+    image_size: int,
     triplet_matrix: np.ndarray,
     concepts: pd.DataFrame,
     non_unique_words: set = None,
+    seed: int = 0,
 ) -> Generator[tuple[list[Image.Image], list[str], np.ndarray], None, None]:
     def get_cls_name(index: int) -> str:
         return concepts.iloc[index]["uniqueID"]
 
     def load_image(cls_name: str) -> Image:
         image_path = os.path.join(image_dir, cls_name + ".jpg")
-        image = Image.open(image_path).resize((64, 64))
+        image = Image.open(image_path).resize((image_size, image_size))
         return image
 
     def not_unique(words: str) -> bool:
@@ -53,13 +105,20 @@ def dataloader(
         n = len(words.intersection(non_unique_words))
         return True if n > 0 else False
 
+    def set_seed(seed):
+        random.seed(seed)
+        np.random.seed(seed)
+        rng = np.random.default_rng(seed)
+        return rng
+
+    rng = set_seed(seed)
+    rng.shuffle(triplet_matrix, axis=0)
     for triplet in triplet_matrix:
         words = list(map(get_cls_name, triplet))
         images = list(map(load_image, words))
 
         if not_unique(words):
             continue
-
         yield images, words, triplet
 
 
@@ -73,67 +132,70 @@ def load_instructions(path: str):
         print(e)
 
 
-def get_image_choice(client, image_triplet, text):
-    # We need to encode the images as base64 strings
+def get_image_query(image_triplet, text):
+    """Content for the GPT 4 image query"""
+
+    def encode_image(image):
+        buffer = io.BytesIO()
+        image.save(buffer, format="PNG")
+        byte_data = buffer.getvalue()
+        base64_str = base64.b64encode(byte_data)
+        return base64_str.decode("utf-8")
+
     content = copy.deepcopy(text)
     content = [{"type": "text", "text": text}]
-
     for image in image_triplet:
         image_encoding = {
             "type": "image_url",
-            "image_url": f"data:image/jpeg;base64,{encode_image(image)}",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{encode_image(image)}",
+                "detail": "low",  # NOTE I set this to low to save on tokens
+            },
         }
         content.append(image_encoding)
+    return content
 
+
+def get_word_query(word_triplet, text):
+    """Content for the GPT 4 word query"""
+    word_triplet = [w.replace("_", " ") for w in word_triplet]
+    task_string = "Execute this task for the following words\n" + ",".join(word_triplet)
+    text += task_string
+    content = [{"type": "text", "text": text}]
+    return content
+
+
+def get_gpt_response(client, content, temperature=0.5, max_tokens=300):
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=[{"role": "user", "content": content}],
-        max_tokens=300,
+        max_tokens=max_tokens,
+        n=1,
+        temperature=temperature,
     )
 
-    choice = response.choices[0].message.content
-    descriptions, choice, position = process_image_response(response)
-    return descriptions, choice, position
+    return response
 
 
 def process_image_response(response):
     choice = response.choices[0].message.content
     descriptions, choice = choice.split("\n")
-    descriptions = descriptions.split(",")
-    for i, d in enumerate(descriptions):
-        d = d.strip()
-        d = d.replace(" ", "_")
-        d = d.lower()
-        descriptions[i] = d
+    descriptions = [
+        d.strip().replace(" ", "_").lower() for d in descriptions.split(",")
+    ]
 
     choice_name, position = choice.split(",")
+    choice_name = choice_name.replace(" ", "_")
     position = int(position) - 1  # convert to 0-based indexing
+
     return descriptions, choice_name, position
 
 
-def get_word_choice(client, word_triplet, text):
-    word_triplet = [w.replace("_", " ") for w in word_triplet]
-    task_string = "Execute this task for the following words\n" + ",".join(word_triplet)
-    text += task_string
-    response = client.chat.completions.create(
-        model="gpt-4-vision-preview",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": text,
-                    }
-                ],
-            }
-        ],
-        max_tokens=300,
-    )
+def process_word_response(response):
     choice = response.choices[0].message.content
     choice, position = choice.split(",")
     position = int(position) - 1  # convert to 0-based indexing
-
+    choice = choice.replace(" ", "_")
     return choice, position
 
 
@@ -145,38 +207,51 @@ def shuffle_data(images: list, words: list, triplet: list):
     return images, words, indices
 
 
-# TODO Still need to figure out why choices are sometimes empty, e.g. why the GPT responses in not correctly parsed.
-# NOTE sometimes I get error message when some input is not allowed...
-# TODO Get it to run first and then outsource the specifics in the TOML file maybe!
+def create_dataloader(
+    image_fp, triplet_fp, concept_fp, non_unique_words_fp, image_size, seed=0
+):
+    """Create a dataloader of the images, words and indices used to probe gpt4"""
+    triplet_matrix = np.loadtxt(triplet_fp, dtype=int)
+    concepts = pd.read_csv(concept_fp, sep="\t")
+    non_unique_words = parse_non_unique_words(non_unique_words_fp)
+    dl = dataloader(
+        image_fp, image_size, triplet_matrix, concepts, non_unique_words, seed
+    )
+
+    return dl
+
+
 # TODO Change all the stupid numpy array and list conver
-def main():
-    image_dir = "./data/images/"
-    train_fp = "./data/trainset.txt"
-    things_concepts = pd.read_csv("./data/things_concepts.tsv", sep="\t")
-    non_unique_words = parse_non_unique_words("./data/NonUniqueWords_edited.xlsx")
-    prompt_images = toml.load("./data/prompt_specifics_images.toml")
-    prompt_words = toml.load("./data/prompt_specifics_words.toml")
+# TODO calculate estimated cost for 1000 triplets
+def main(args):
+    dataloader = create_dataloader(
+        args.image_dir,
+        args.train_fp,
+        args.concepts_fp,
+        args.non_unique_words_fp,
+        args.image_size,
+        args.seed,
+    )
 
-    image_triplets = np.loadtxt(train_fp, dtype=int)
-
-    random.seed(0)
-    rng = np.random.default_rng(0)
-    rng.shuffle(image_triplets, axis=0)
-
+    prompt_images = toml.load(args.prompt_images_fp)
+    prompt_words = toml.load(args.prompt_words_fp)
     client = OpenAI()
-    dl = dataloader(image_dir, image_triplets, things_concepts, non_unique_words)
-    max_iterations = 20
+
+    metadata = copy.deepcopy(vars(args))
+    for key in [
+        "config",
+        "root_table",
+        "max_triplets",
+        "output_fp",
+    ]:
+        metadata.pop(key)
+
     responses = []
-    i = 0
-    for image_triplet, word_triplet, human_indices in dl:
-        # try:
+    for i, (image_triplet, word_triplet, human_indices) in enumerate(dataloader):
         # We shuffle the triplet since we take the triplet indices from behavior, where we store
         # the odd one out decision always in the last position and we want to make sure that the ordering
         # is random and not related to the position of the odd one out as identified from human behavior
-        print("Process triplet", i)
-        i += 1
-        # if i != 8:
-        #     continue
+        print(f"Process triplet {i} / {args.max_triplets}")
 
         triplet_responses = dict()
         triplet_responses["human_indices"] = list(human_indices)
@@ -188,18 +263,20 @@ def main():
             image_triplet, word_triplet, human_indices
         )
 
-        (
-            image_descriptions,
-            image_choice,
-            image_position,
-        ) = get_image_choice(client, image_triplet, prompt_images["prompt"])
+        image_query = get_image_query(image_triplet, prompt_images["prompt"])
+        word_query = get_word_query(word_triplet, prompt_words["prompt"])
 
-        word_choice, word_position = get_word_choice(
-            client, word_triplet, prompt_words["prompt"]
+        image_response = get_gpt_response(
+            client, image_query, args.temperature, max_tokens=300
+        )
+        word_response = get_gpt_response(
+            client, word_query, args.temperature, max_tokens=300
         )
 
-        word_choice = word_choice.replace(" ", "_")
-        image_choice = image_choice.replace(" ", "_")
+        image_descriptions, image_choice, image_position = process_image_response(
+            image_response
+        )
+        word_choice, word_position = process_word_response(word_response)
 
         # We know move the index of the odd one out to the last position
         ooo_word = list(np.roll(gpt_indices, -word_position))
@@ -215,19 +292,17 @@ def main():
         triplet_responses["gpt_word_ooo"] = ooo_word[-1]
         triplet_responses["gpt_word_choice"] = word_choice
 
-        print(word_triplet, image_descriptions)
-
         responses.append(triplet_responses)
         df = pd.DataFrame(responses, columns=triplet_responses.keys())
-        df.to_pickle("./data/gpt4_choice_words_images.pkl")
 
-        # except Exception as e:
-        #     print(e)
-        #     continue
+        save_dict = {"metadata": metadata, "dataframe": df}
+        with open(args.output_fp, "wb") as file:
+            pickle.dump(save_dict, file)
 
-        if i == max_iterations:
+        if i == args.max_triplets - 1:
             break
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
